8d01b93cdf1bac646cfbffca8a516d581376c714
Pedro Alves <pmgalves@gmail.com>
2011-10-08 23:45:21 +0100

    - Switched saiku to 2.1
    Signed-off-by: Pedro Alves <pmgalves@gmail.com>

--- ctools-installer.sh
+++ ctools-installer.sh
@@ -3,7 +3,7 @@
 echo
 echo CTOOLS
 echo
-echo ctools-installer version 1.5
+echo ctools-installer version 1.6
 echo 
 echo "Author: Pedro Alves (webdetails)"
 echo Thanks to Analytical Labs for jenkins builds
@@ -12,6 +12,7 @@ echo
 echo 
 echo Changelog:
 echo
+echo v1.6 - Changed saiku download path to 2.1
 echo v1.5 - Changed default indicator values in prompts
 echo v1.4 - Added Saiku for the list of installs
 echo v1.3 - Added support for automatic updates
@@ -90,8 +91,10 @@ unzip .tmp/cde/dist.zip -d .tmp > /dev/null
 
 # SAIKU
 
-wget --no-check-certificate 'http://ci.analytical-labs.com/jenkins/job/saiku-plugin/lastSuccessfulBuild/artifact/saiku-bi-platform-plugin/target/saiku-plugin-*zip*/*zip*/target.zip' -P .tmp/saiku -o /dev/null
-unzip .tmp/saiku/target.zip -d .tmp > /dev/null
+#wget --no-check-certificate 'http://ci.analytical-labs.com/jenkins/job/saiku-plugin/lastSuccessfulBuild/artifact/saiku-bi-platform-plugin/target/saiku-plugin-*zip*/*zip*/target.zip' -P .tmp/saiku -o /dev/null
+#unzip .tmp/saiku/target.zip -d .tmp > /dev/null
+# tamporarily switch for 2.1
+wget --no-check-certificate 'http://analytical-labs.com/downloads/saiku-plugin-2.1.zip' -P .tmp -o /dev/null
 
 echo
 echo -n "Installing CDA. This will delete everything in $SOLUTION_DIR/system/cda. you sure? (y/N) "
