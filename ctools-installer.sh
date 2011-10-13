e5ce24ed0b52d1ca54a1aae75fde24b057d797ff
Pedro Alves <pmgalves@gmail.com>
2011-10-13 15:09:10 +0100

    - v1.7: Changed url locations to new path of AL
    Signed-off-by: Pedro Alves <pmgalves@gmail.com>

--- ctools-installer.sh
+++ ctools-installer.sh
@@ -3,7 +3,7 @@
 echo
 echo CTOOLS
 echo
-echo ctools-installer version 1.6
+echo ctools-installer version 1.7
 echo 
 echo "Author: Pedro Alves (webdetails)"
 echo Thanks to Analytical Labs for jenkins builds
@@ -12,6 +12,7 @@ echo
 echo 
 echo Changelog:
 echo
+echo v1.7 - Changed url locations to new path of analytical labs
 echo v1.6 - Changed saiku download path to 2.1
 echo v1.5 - Changed default indicator values in prompts
 echo v1.4 - Added Saiku for the list of installs
@@ -76,22 +77,22 @@ echo
 
 
 # CDF
-wget --no-check-certificate http://ci.analytical-labs.com/jenkins/job/Webdetails-CDF/lastSuccessfulBuild/artifact/bi-platform-v2-plugin/dist/pentaho-cdf-TRUNK-SNAPSHOT.zip -P .tmp/dist/ -o /dev/null
+wget --no-check-certificate http://ci.analytical-labs.com/job/Webdetails-CDF/lastSuccessfulBuild/artifact/bi-platform-v2-plugin/dist/pentaho-cdf-TRUNK-SNAPSHOT.zip -P .tmp/dist/ -o /dev/null
 
 
 # CDA
-wget --no-check-certificate 'http://ci.analytical-labs.com/jenkins/job/Webdetails-CDA/lastSuccessfulBuild/artifact/dist/*zip*/dist.zip' -P .tmp/cda -o /dev/null
+wget --no-check-certificate 'http://ci.analytical-labs.com/job/Webdetails-CDA/lastSuccessfulBuild/artifact/dist/*zip*/dist.zip' -P .tmp/cda -o /dev/null
 unzip .tmp/cda/dist.zip -d .tmp > /dev/null
 
 
 # CDE
-wget --no-check-certificate 'http://ci.analytical-labs.com/jenkins/job/Webdetails-CDE/lastSuccessfulBuild/artifact/server/plugin/dist/*zip*/dist.zip' -P .tmp/cde -o /dev/null
+wget --no-check-certificate 'http://ci.analytical-labs.com/job/Webdetails-CDE/lastSuccessfulBuild/artifact/server/plugin/dist/*zip*/dist.zip' -P .tmp/cde -o /dev/null
 unzip .tmp/cde/dist.zip -d .tmp > /dev/null
 
 
 # SAIKU
 
-#wget --no-check-certificate 'http://ci.analytical-labs.com/jenkins/job/saiku-plugin/lastSuccessfulBuild/artifact/saiku-bi-platform-plugin/target/saiku-plugin-*zip*/*zip*/target.zip' -P .tmp/saiku -o /dev/null
+#wget --no-check-certificate 'http://ci.analytical-labs.com/job/saiku-plugin/lastSuccessfulBuild/artifact/saiku-bi-platform-plugin/target/saiku-plugin-*zip*/*zip*/target.zip' -P .tmp/saiku -o /dev/null
 #unzip .tmp/saiku/target.zip -d .tmp > /dev/null
 # tamporarily switch for 2.1
 wget --no-check-certificate 'http://analytical-labs.com/downloads/saiku-plugin-2.1.zip' -P .tmp -o /dev/null
